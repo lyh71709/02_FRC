6a3f69e6264bbf59eba6545c065903e67845162b
lyh71709 <60583161+lyh71709@users.noreply.github.com>
2021-05-04 09:55:10 +1200

    Finished Number checker

--- 01_number_checker.py
+++ 01_number_checker.py
@@ -1,1 +1,24 @@
-hi
+# Number checker function goes here
+# Checks that it is not 0
+def number_checker(question, error, num_type):
+    valid = False
+    while not valid:
+
+        try:
+            response = num_type(input(question))
+        
+            if response <= 0:
+                print(error)
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+# Main routine
+get_int = number_checker("How many do you need? ", "Please enter an amount more than 0", int)
+
+get_cost = number_checker("How much does it cost? $", "Please enter an amount more than 0", float)
+
+print("You need {}" .format(get_int))
+print("It costs ${:.2f}".format(get_cost))
